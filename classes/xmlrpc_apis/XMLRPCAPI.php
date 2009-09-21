65cf8dd7741ad7a874722d91e1209d19b5a4be9b
Jingsi Zhu <zhu@jing.si>
2009-09-21 12:09:44 +0000

    * XMLRPC API Module

---
+++ classes/xmlrpc_apis/XMLRPCAPI.php
@@ -0,0 +1,9 @@
+<?php
+class XMLRPCAPI
+{
+	public function dullArray($arg)
+	{
+	}
+	
+}
+?>
--- xmlrpc.php
+++ xmlrpc.php
@@ -10,4 +10,37 @@ require_once 'init.php';
 
 require_once ROOT . 'classes/xmlrpc/xmlrpc.inc';
 require_once ROOT . 'classes/xmlrpc/xmlrpcs.inc';
+require_once ROOT . 'classes/xmlrpc_apis/XMLRPCAPI.php';
+require_once ROOT . 'classes/xmlrpc_apis/WPXMLRPCAPI.php';
+
+$server = new xmlrpc_server(array(
+			'wp.getUsersBlogs'		=> 'this:wp_getUsersBlogs',
+			'wp.getPage'			=> 'this:wp_getPage',
+			'wp.getPages'			=> 'this:wp_getPages',
+			'wp.newPage'			=> 'this:wp_newPage',
+			'wp.deletePage'			=> 'this:wp_deletePage',
+			'wp.editPage'			=> 'this:wp_editPage',
+			'wp.getPageList'		=> 'this:wp_getPageList',
+			'wp.getAuthors'			=> 'this:wp_getAuthors',
+			'wp.getCategories'		=> 'this:mw_getCategories',		// Alias
+			'wp.getTags'			=> 'this:wp_getTags',
+			'wp.newCategory'		=> 'this:wp_newCategory',
+			'wp.deleteCategory'		=> 'this:wp_deleteCategory',
+			'wp.suggestCategories'	=> 'this:wp_suggestCategories',
+			'wp.uploadFile'			=> 'this:mw_newMediaObject',	// Alias
+			'wp.getCommentCount'	=> 'this:wp_getCommentCount',
+			'wp.getPostStatusList'	=> 'this:wp_getPostStatusList',
+			'wp.getPageStatusList'	=> 'this:wp_getPageStatusList',
+			'wp.getPageTemplates'	=> 'this:wp_getPageTemplates',
+			'wp.getOptions'			=> 'this:wp_getOptions',
+			'wp.setOptions'			=> 'this:wp_setOptions',
+			'wp.getComment'			=> 'this:wp_getComment',
+			'wp.getComments'		=> 'this:wp_getComments',
+			'wp.deleteComment'		=> 'this:wp_deleteComment',
+			'wp.editComment'		=> 'this:wp_editComment',
+			'wp.newComment'			=> 'this:wp_newComment',
+			'wp.getCommentStatusList' => 'this:wp_getCommentStatusList',
+
+));
+
 ?>
